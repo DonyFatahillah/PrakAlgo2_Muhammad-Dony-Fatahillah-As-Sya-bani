5547ae0c13552b11142259e8e3200f75809430d3
DonyFatahillah <145980954+DonyFatahillah@users.noreply.github.com>
2023-09-25 17:22:41 +0700

    Add files via upload

---
+++ Prak2_elkom2_Dony.py
@@ -0,0 +1,16 @@
+print("\n \n")
+
+print("@@@@   @@@@@   @@  @  @   @")
+print("@   @  @   @   @ @ @   @ @")
+print("@   @  @   @   @  @@    @")
+print("@@@@   @@@@@   @   @    @")
+
+print("\n \n")
+
+
+angkaPertama = int(input("masukan angka pertama : "))
+angkaKedua = int(input("masukan angka kedua : "))
+angkaKetiga = int(input("masukan angka ketiga : "))
+
+hasil = max(angkaPertama, angkaKedua, angkaKetiga)
+print("angka terbesar adalah ", hasil)
